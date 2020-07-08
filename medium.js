5f9fde9bb40ef54e49847f1980627cb27df7aa3c
Jeremiah <jscott4720@gmail.com>
2020-07-08 19:51:19 -0400

    made medium and hard answers clickable

--- medium.js
+++ medium.js
@@ -1,1 +1,15 @@
 console.log("This is the medium level")
+
+
+let answers = document.querySelectorAll("p")
+
+console.log(answers)
+
+for (i = 0; i < answers.length; i++) {
+    let buttons = answers[i]
+    buttons.addEventListener("click", beenClicked)
+
+    function beenClicked(){
+        console.log("I'm clicked")
+    }
+}
