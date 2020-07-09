233ecdb9681060d047ec65816b2d23fd10b39d9a
Jeremiah <jscott4720@gmail.com>
2020-07-08 22:20:25 -0400

    added data sets....in the wrong place

--- easy.js
+++ easy.js
@@ -8,7 +8,7 @@ for (i = 0; i < answers.length; i++) {
     let buttons = answers[i]
     buttons.addEventListener("click", beenClicked)
 
-    function beenClicked(){
-        console.log("I'm clicked")
+    function beenClicked(e){
+        console.log(e)
     }
 }
