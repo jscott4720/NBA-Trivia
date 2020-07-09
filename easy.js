5c54981af1d3efeb430eff05d56f55c8f6c8483e
Jeremiah <jscott4720@gmail.com>
2020-07-08 22:25:45 -0400

    Added data sets in the RIGHT place and deleted the old ones

--- easy.js
+++ easy.js
@@ -2,6 +2,8 @@ console.log("This is the easy level")
 
 let answers = document.querySelectorAll("p")
 
+let score = 0 
+
 console.log(answers)
 
 for (i = 0; i < answers.length; i++) {
@@ -10,5 +12,9 @@ for (i = 0; i < answers.length; i++) {
 
     function beenClicked(e){
         console.log(e)
+        if (e.target.attributes[0].nodeValue == "Correct") {
+            score+=10
+        }
     }
 }
+
